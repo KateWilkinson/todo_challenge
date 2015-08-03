cd86b7ab8810c6c0ec2580bbb5bfedda4ed659b4
KateWilkinson <kate.wilkinson@hotmail.com>
2015-08-03 13:13:32 +0100

    updated config

--- test/e2e/conf.js
+++ test/e2e/conf.js
@@ -1,9 +1,4 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['toDoFeature.js'],
-  multiCapabilities: [{
-     browserName: 'firefox'
-  }, {
-   browserName: 'chrome'
-  }]
+  specs: ['toDoFeature.js']
 };
